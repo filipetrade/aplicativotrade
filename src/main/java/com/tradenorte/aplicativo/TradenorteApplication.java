d8c659f2af7d4bcce906956ac9f1eccae3985d09
Filipe Tavares <filipetrade@gmail.com>
2018-04-25 20:04:10 -0300

    Projeto criado 25/04/2018 20h04

---
+++ src/main/java/com/tradenorte/aplicativo/TradenorteApplication.java
@@ -0,0 +1,12 @@
+package com.tradenorte.aplicativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradenorteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradenorteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tradenorte/aplicativo/TradenorteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tradenorte.aplicativo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TradenorteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
